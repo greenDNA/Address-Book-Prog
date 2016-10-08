549c763a293ad22c2cf5efdb9cb42417358398ca
greenDNA <trayvont.york@gmail.com>
2016-10-08 15:43:06 -0400

    Projected integrated with intelliJ

--- Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-
-    public static void main(String[] args) {
-        BookUser user = new BookUser();
-        user.options(user.getChoice());
-        user = null;
-        return;
-    }
-}
---
+++ src/Main.java
@@ -0,0 +1,13 @@
+package src;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        BookUser user = new BookUser();
+        user.options(user.getChoice());
+        user = null;
+        return;
+
+    }
+}
